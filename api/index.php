a3b9302f95576001d0ae387f13da913a6ae1f4af
amina <hoquemoni027@gmail.com>
2026-09-13 23:53:22 +0600

    Fix Vercel tmp storage setup

--- api/index.php
+++ api/index.php
@@ -1,2 +1,26 @@
-<?php
+ <?php
+
+// Create required directories in Vercel's writable /tmp directory
+$dirs = [
+    '/tmp/storage/bootstrap/cache',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/cache',
+];
+
+foreach ($dirs as $dir) {
+    if (!file_exists($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
+// Set storage and bootstrap cache paths to /tmp
+$_ENV['APP_STORAGE_PATH'] = '/tmp/storage';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/storage/bootstrap/cache/config.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/storage/bootstrap/cache/events.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/storage/bootstrap/cache/packages.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/storage/bootstrap/cache/routes.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/storage/bootstrap/cache/services.php';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+
 require __DIR__ . '/../public/index.php';
